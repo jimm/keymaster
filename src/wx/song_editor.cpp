add08043d7dc988151779cf3acff4ba702cb4027
Jim Menard <jim.menard@warbyparker.com>
2020-08-02 19:58:43 -0400

    gui tweaks

--- src/wx/song_editor.cpp
+++ src/wx/song_editor.cpp
@@ -3,8 +3,11 @@
 #include "../keymaster.h"
 #include "../song.h"
 
+#define TEXT_EDIT_HEIGHT 22
+#define NAME_WIDTH 250
+#define NAME_HEIGHT TEXT_EDIT_HEIGHT
 #define BPM_WIDTH 52
-#define BPM_HEIGHT 20
+#define BPM_HEIGHT TEXT_EDIT_HEIGHT
 
 wxBEGIN_EVENT_TABLE(SongEditor, wxDialog)
   EVT_BUTTON(wxID_OK, SongEditor::save)
@@ -18,19 +21,19 @@ SongEditor::SongEditor(wxWindow *parent, Song *song_ptr)
   wxSizerFlags panel_flags = wxSizerFlags().Border(wxTOP|wxLEFT|wxRIGHT);
   sizer->Add(make_name_panel(this), panel_flags);
   sizer->Add(make_clock_panel(this), panel_flags);
-  sizer->Add(CreateStdDialogButtonSizer(wxOK | wxCANCEL));
+  sizer->Add(CreateStdDialogButtonSizer(wxOK | wxCANCEL), panel_flags);
   SetSizerAndFit(sizer);
 }
 
 wxWindow *SongEditor::make_name_panel(wxWindow *parent) {
   wxPanel *p = new wxPanel(parent, wxID_ANY);
   wxBoxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
-  wxSizerFlags center_flags =
-    wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL);
+  wxSizerFlags panel_flags =
+    wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL).Border(wxTOP|wxLEFT|wxRIGHT);
 
-  sizer->Add(new wxStaticText(p, wxID_ANY, "Name"), center_flags);
-  name_text = new wxTextCtrl(p, ID_SE_Name, song->name, wxDefaultPosition);
-  sizer->Add(name_text, center_flags);
+  sizer->Add(new wxStaticText(p, wxID_ANY, "Name "), panel_flags);
+  name_text = new wxTextCtrl(p, ID_SE_Name, song->name, wxDefaultPosition, wxSize(NAME_WIDTH, NAME_HEIGHT));
+  sizer->Add(name_text, panel_flags);
 
   p->SetSizerAndFit(sizer);
   return p;
@@ -40,20 +43,20 @@ wxWindow *SongEditor::make_clock_panel(wxWindow *parent) {
   wxPanel *p = new wxPanel(parent, wxID_ANY);
   wxBoxSizer *outer_sizer = new wxBoxSizer(wxVERTICAL);
   wxBoxSizer *field_sizer = new wxBoxSizer(wxHORIZONTAL);
-  wxSizerFlags center_flags =
-    wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL);
+  wxSizerFlags panel_flags =
+    wxSizerFlags().Align(wxALIGN_CENTER_VERTICAL).Border(wxTOP|wxLEFT|wxRIGHT);
 
   char buf[16];
   format_float(song->bpm, buf);
   text_bpm = new wxTextCtrl(p, ID_SE_BPM, buf, wxDefaultPosition, wxSize(BPM_WIDTH, BPM_HEIGHT));
-  field_sizer->Add(text_bpm, center_flags);
-  field_sizer->Add(new wxStaticText(p, wxID_ANY, "BPM"), center_flags);
+  field_sizer->Add(text_bpm, panel_flags);
+  field_sizer->Add(new wxStaticText(p, wxID_ANY, "BPM"), panel_flags);
 
   cb_clock_start = new wxCheckBox(p, ID_SE_StartClock, "Start Clock");
   cb_clock_start->SetValue(song->clock_on_at_start);
-  field_sizer->Add(cb_clock_start, center_flags);
+  field_sizer->Add(cb_clock_start, panel_flags);
 
-  outer_sizer->Add(new wxStaticText(p, wxID_ANY, "Clock"));
+  outer_sizer->Add(new wxStaticText(p, wxID_ANY, "Clock "));
   outer_sizer->Add(field_sizer);
 
   p->SetSizerAndFit(outer_sizer);
