2cafb5c6f80278d260935641bc4a545212a5e70a
Jim Menard <jim@jimmenard.com>
2020-10-12 18:12:23 -0400

    improve observable removal

--- src/observable.cpp
+++ src/observable.cpp
@@ -1,11 +1,19 @@
 #include "observable.h"
 
 void Observable::add_observer(Observer *o) {
+  for (auto observer : observers)
+    if (observer == o)
+      return;
   observers.push_back(o);
 }
 
 void Observable::remove_observer(Observer *o) {
-  std::remove(observers.begin(), observers.end(), o);
+  for (vector<Observer *>::iterator i = observers.begin(); i != observers.end(); ++i) {
+    if (*i == o) {
+      observers.erase(i);
+      return;
+    }
+  }
 }
 
 void Observable::changed(void *arg) {
