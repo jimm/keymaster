fa9e5c6bdca3b654a7d3cfdcd241f7e25cc929c8
Jim Menard <jmenard@seatgeek.com>
2023-09-19 08:13:27 -0400

    removed unused command line arg

--- src/wx/app.cpp
+++ src/wx/app.cpp
@@ -7,7 +7,6 @@
 
 static const wxCmdLineEntryDesc g_cmdLineDesc [] = {
   { wxCMD_LINE_SWITCH, "l", "list-devices", "Display MIDI Devices" },
-  { wxCMD_LINE_SWITCH, "i", "initialize", "Output initial KeyMaster file" },
   { wxCMD_LINE_PARAM, nullptr, nullptr, "KeyMaster file", wxCMD_LINE_VAL_STRING,
     wxCMD_LINE_PARAM_OPTIONAL },
   { wxCMD_LINE_NONE }
